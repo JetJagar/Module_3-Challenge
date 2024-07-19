e6aa8d7cebb0f51a0a4a3bf9f035bdf61e300b40
Carl Mini <154369511+JetJagar@users.noreply.github.com>
2024-07-18 23:11:59 -0500

    Update and rename main_2.py to py_poll.py

--- py_poll.py
+++ py_poll.py
@@ -1,17 +1,22 @@
+# imported modules to run analysis
 import os as os
 import pandas as pd
+# setting the path to retrieve the csv file
 csvpath_2 = os.path.join("Resources", "election_data.csv")
-
+# creating a data frame to read the raw csv 
 df = pd.read_csv("resources/election_data.csv")
-# df.info()
+# get the total number of values (rows) in the data frame 
 total = len(df)
-# print(df.keys())
+# creating a data frame to house value counts preformed on the original data frame
+# its is sorted from highest to smallest values
 candidate_count_df = (df["Candidate"].value_counts(ascending = False))
 print("Election Results")
 print("-------------------------")
 print("Total Votes: {} ".format(total))
 print("-------------------------")
+# sorted the items in the data frame to be sorted alphabetically 
 for candidate in sorted(candidate_count_df.items()):
+    # {} shows what value to be printed and in what order 
     print("{}: {}% ({})".format(candidate[0], round(candidate[1]/total * 100, 2),candidate[1]))
 print("-------------------------")
 # next returns only the first item in a list
